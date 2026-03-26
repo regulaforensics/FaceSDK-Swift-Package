1b9f19e7feaa56f0b3a47899a14e5ced2a8d0ce4
Jenkins <jenkins@regula.by>
2026-03-26 23:50:32 +0300

    Release 8.1.3935

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
             targets: ["\(packageName)Common"]),
     ],
     dependencies: [
-        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "9.2.1988"))),
+        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "9.3.2092"))),
     ],
     targets: [
-        .binaryTarget(name: "FaceSDK", url: "https://pods.regulaforensics.com/FaceSDK/8.1.3731/FaceSDK-8.1.3731.zip", checksum: "55d84778a9fba2ecaec8aae584455d740c026c921267d74307cbbafe608a5eaf"),
+        .binaryTarget(name: "FaceSDK", url: "https://pods.regulaforensics.com/FaceSDK/8.1.3935/FaceSDK-8.1.3935.zip", checksum: "a4698704f254fddc4b7a171ddb7d31ff17ef570527ed276d014174fb37e9673f"),
         .target(
             name: "\(packageName)Common",
             dependencies: [
