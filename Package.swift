83d799c88445c547b31909101d84789c5a07c9ff
Jenkins <jenkins@regula.by>
2026-07-28 13:59:03 +0300

    Nightly 8.3.4686

--- Package.swift
+++ Package.swift
@@ -1,15 +1,29 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let packageName = "FaceSDK"
+
 let package = Package(
     name: "FaceSDK",
     platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "FaceSDK",
-            targets: ["FaceSDKStage"]),
+            targets: ["\(packageName)Common"]),
+    ],
+    dependencies: [
+        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "9.7.2595-nightly"))),
     ],
     targets: [
-        .binaryTarget(name: "FaceSDKStage", url: "https://pods.regulaforensics.com/Stage/FaceSDKStage/8.3.4614/FaceSDKStage-8.3.4614.zip", checksum: "bfaa2af75470146357965ac1840ad0ce66fa0dd10afd85e91f3e6b37bd5a7b98"),
+        .binaryTarget(name: "FaceSDK", url: "https://pods.regulaforensics.com/Nightly/FaceSDKNightly/8.3.4686/FaceSDKNightly-8.3.4686.zip", checksum: "45529cc684020b17bcae432cec0b5b071e893be7c863fb91e05aed0dd57b9adf"),
+        .target(
+            name: "\(packageName)Common",
+            dependencies: [
+                .target(name: "FaceSDK"),
+                .product(name: "RegulaCommon", package: "RegulaCommon")
+            ],
+            path: "Sources",
+            sources: ["dummy.swift"]
+        )
     ]
 )
