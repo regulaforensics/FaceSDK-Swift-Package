b941e1e1cc819435252b13673d21cf468b8ec4a6
Jenkins <jenkins@regula.by>
2026-09-24 03:29:26 +0300

    Nightly 8.4.4970

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "9.9.2861-nightly"))),
     ],
     targets: [
-        .binaryTarget(name: "FaceSDK", url: "https://pods.regulaforensics.com/Nightly/FaceSDKNightly/8.3.4969/FaceSDKNightly-8.3.4969.zip", checksum: "c1f81a5c0d9bda2037b8abb0ea25577ca3b8a9925ccfcde9ea2e6f9e1f2ae4e3"),
+        .binaryTarget(name: "FaceSDK", url: "https://pods.regulaforensics.com/Nightly/FaceSDKNightly/8.4.4970/FaceSDKNightly-8.4.4970.zip", checksum: "2b331f158430635885fe102a77f943a6f74ac8743f7ab73608d609b80be1e995"),
         .target(
             name: "\(packageName)Common",
             dependencies: [
